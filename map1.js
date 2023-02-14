b4652774c5c75e53b0b058c1ea930d612c5b28ba
Eduardo Nery Campos <duduguima12@gmail.com>
2023-02-13 23:02:02 -0300

    Add files via upload

---
+++ map1.js
@@ -0,0 +1,9 @@
+const nums = [1, 2, 3, 4, 5]
+
+// For com prpósito
+
+let resultado = nums.map(function(e){
+    return e * 2
+})
+console.log(resultado)
+//o Map gera um novo array, e nesse caso com todos os valores multiplicados por dois
